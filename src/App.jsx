1f02646de5e013c97cef9327ea8232972ee77178
kartic71197 <karticmalhotra3289@gmail.com>
2025-04-13 18:06:59 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex gap-3 items-center justify-start p-3"> 
+      Count:{count}
+      <button
+        className="p-3 bg-gray-400 text-black rounded-lg"
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        className="p-3 bg-gray-400 text-black rounded-lg"
+        onClick={() => {
+          setCount((prev) => prev - 1);
+        }}
+      >
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
